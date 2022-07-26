27c5c267cd711ee5fb2f403c09a6acc8e571045d
s-alih <slh32064@gmail.com>
2022-07-27 01:51:34 +0530

    .ev

--- deploy.js
+++ deploy.js
@@ -6,6 +6,12 @@ async function main() {
   //http://127.0.0.1:7545
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  // const encryptedJson = fs.readFileSync("./.encryptedKey.json");
+  // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+  //   encryptedJson,
+  //   process.env.PASSWORD
+  // );
+  // wallet = await wallet.connect(provider);
 
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
