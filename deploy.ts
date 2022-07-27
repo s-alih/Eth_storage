a1253378704dd4e7ab70196f4d0bb2c9ae5f5e2b
s-alih <slh32064@gmail.com>
2022-07-28 02:28:39 +0530

    typescript migration

--- deploy.ts
+++ deploy.ts
@@ -1,11 +1,11 @@
-const ethers = require("ethers")
-const fs = require("fs-extra")
-require("dotenv").config()
+import { ethers } from "ethers"
+import * as fs from "fs-extra"
+import "dotenv/config"
 
 async function main() {
     //http://127.0.0.1:7545
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
     // const encryptedJson = fs.readFileSync("./.encryptedKey.json");
     // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
     //   encryptedJson,
